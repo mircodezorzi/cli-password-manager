75e8a5867433531407aa23849dfd9d16e26729d5
Mirco De Zorzi <mircodezorzi@gmail.com>
2018-02-12 22:16:18 +0100

    Inhereting crypto class

--- manager.cpp
+++ manager.cpp
@@ -1,5 +1,4 @@
 #include "manager.h"
-#include "logger.h"
 
 #include "exceptions.h"
 
@@ -11,45 +10,46 @@
 #include <iostream>
 
 Manager::Manager(std::string dataFilepath, std::string logFilpath, size_t logThresholdLevel, std::string LogMessageFormat) 
-    :Logger(logFilpath, logThresholdLevel, LogMessageFormat){
-
+    : Logger(logFilpath, logThresholdLevel, LogMessageFormat)
+    , Crypto(){
     mPath = dataFilepath;
 }
 
 void Manager::readData(){
     size_t sitePos[2], userPos[2], passPos[2], endPos;
     std::vector<std::string> rowData;
+    std::string decryptedData;
     
     std::ifstream inputFile(mPath); 
     std::string encryptedData ((std::istreambuf_iterator<char>(inputFile)),\
 				std::istreambuf_iterator<char>());
     
     std::cout << encryptedData;
-    mDecryptedData = encryptedData;
+    decryptedData= encryptedData;
     
     do{
-	sitePos[0] = mDecryptedData.find("\"");
-	sitePos[1] = mDecryptedData.find("\"", sitePos[0] + 1);
+	sitePos[0] = decryptedData.find("\"");
+	sitePos[1] = decryptedData.find("\"", sitePos[0] + 1);
 
-	userPos[0] = mDecryptedData.find("\"", sitePos[1] + 1);
-	userPos[1] = mDecryptedData.find("\"", userPos[0] + 1); 
+	userPos[0] = decryptedData.find("\"", sitePos[1] + 1);
+	userPos[1] = decryptedData.find("\"", userPos[0] + 1); 
     
-	passPos[0] = mDecryptedData.find("\"", userPos[1] + 1);	
-	passPos[1] = mDecryptedData.find("\"", passPos[0] + 1);
+	passPos[0] = decryptedData.find("\"", userPos[1] + 1);	
+	passPos[1] = decryptedData.find("\"", passPos[0] + 1);
     
-	endPos = mDecryptedData.find(";");
+	endPos = decryptedData.find(";");
 
-	rowData.push_back(mDecryptedData.substr(sitePos[0] + 1, sitePos[1] - sitePos[0] - 1));
-	rowData.push_back(mDecryptedData.substr(userPos[0] + 1, userPos[1] - userPos[0] - 1));
-	rowData.push_back(mDecryptedData.substr(passPos[0] + 1, passPos[1] - passPos[0] - 1));
+	rowData.push_back(decryptedData.substr(sitePos[0] + 1, sitePos[1] - sitePos[0] - 1));
+	rowData.push_back(decryptedData.substr(userPos[0] + 1, userPos[1] - userPos[0] - 1));
+	rowData.push_back(decryptedData.substr(passPos[0] + 1, passPos[1] - passPos[0] - 1));
 	mData.push_back(rowData);
 	
     	rowData.clear();
-	mDecryptedData.erase(0, endPos + 1);
+	decryptedData.erase(0, endPos + 1);
 
-    }while(!mDecryptedData.empty());
+    }while(!decryptedData.empty());
 
-    info();
+    //info();
     // prints out the mData array, can be deleted
     for(std::vector<std::string> i : mData){
 	for(std::string j : i)
@@ -60,11 +60,11 @@ void Manager::readData(){
 }
 
 void Manager::writeData(){
-    std::ofstream outputFile(mPath);
+    /*std::ofstream outputFile(mPath);
     for(std::vector<std::string> i : mData){
 	for(std::string j : i){
-	    mDecryptedData += "\"" + j + "\" ";
+	    decryptedData += "\"" + j + "\" ";
 	}
-	mDecryptedData += "; ";
-    }
+	decryptedData += "; ";
+    }*/
 }
--- manager.h
+++ manager.h
@@ -2,25 +2,25 @@
 #define MANAGER_H_FSNLIKSO
 
 #include "logger.h"
+#include "crypto.h"
 
 #include <vector>
 #include <string>
 
-class Manager : private Logger{
+class Manager : private Logger, public Crypto{
 
 public:
     Manager(std::string dataFilepath = "data.crypt", std::string logFilepath = "out.log",\
 	    size_t logThersholdLevel = 0, std::string LogMessageFormat = "%(date) - %(time) - %(level) - %(msg)");
   
     void setFilepath(std::string dataFilepath) {mPath = dataFilepath;}
-    void setLogFilpath(std::string logFilpath) {mPath = }
+    void setLogFilpath(std::string logFilepath) {mPath = logFilepath;}
 
     void readData();
     void writeData();
 
 private:
     std::vector< std::vector<std::string> > mData;
-
     std::string mPath;
 };
 
