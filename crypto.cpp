a4c576e5a8521778a370f6bef645b562c405107c
Mirco De Zorzi <mircodezorzi@gmail.com>
2018-02-13 23:53:22 +0100

    Worked on encrypt function

--- crypto.cpp
+++ crypto.cpp
@@ -4,30 +4,90 @@
 #include <time.h>
 #include <bits/stdc++.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <algorithm>
 
-Crypto::Crypto(){
-    generateKey();
+
+Crypto::Crypto(std::string key, size_t keyLength){
+    if(key.empty()) generateKey();
+    else	    mKey = key;
+
+    mKeyLength = keyLength;
 }
 
 void Crypto::generateKey(){
     srand(time(NULL));
-    mKey = std::to_string(rand());
+    for (int i = 0; i < mKeyLength; i++)
+	mKey += rand() % 26 + 97; 
+}
+
+std::vector<bool> Crypto::toByte(char character){
+    std::vector<bool> result;
+    for(int i = 0; i < 8; i++){
+	result.push_back(character % 2);
+	character /= 2;
+    }
+    std::reverse(result.begin(), result.end());
+    return result;
+}
+
+char Crypto::toChar(std::vector<bool> byte){
+    int result = 0;
+    for(int i = 0; i < 8; i++)
+	result += byte[i] * pow(2, 8 - i);
+    return result;
 }
 
 void Crypto::encrypt(std::string &rData){
-    std::string result = "";
-    char dataArray[rData.length()];
-    strcpy(dataArray, rData.c_str());
-    for(char i : dataArray)
-	result += i + 1;	
-    rData = result;
+    std::string encrypted_data = "";
+    std::vector<bool> binary_character, binary_key, binary_result;
+    
+    char data_array[rData.length()];
+    strcpy(data_array, rData.c_str());
+
+    char key_array[mKey.length()];
+    strcpy(key_array, mKey.c_str());
+    
+    for(int i = 0; i < sizeof(data_array); i++){
+	
+	binary_character = toByte(data_array[i]);
+	binary_key       = toByte(key_array[i % sizeof(key_array)]);
+
+	for(int j = 0; j < 8; j++)
+	    binary_result.push_back(binary_character[j] ^ binary_key[j]); 
+	 
+	encrypted_data += toChar(binary_result);
+	std::cout << std::endl << "data:   ";
+	for(auto n:binary_character)
+	    std::cout << n;
+	std::cout << std::endl << "key:    ";
+	for(auto n:binary_key)
+	    std::cout << n;
+	std::cout << std::endl << "result: ";
+	for(auto n:binary_result)
+	    std::cout << n;
+	std::cout << toChar(binary_result);
+	binary_key.clear();
+	binary_character.clear();
+	binary_result.clear();
+    }
+    rData = encrypted_data;
 }
 
 void Crypto::decrypt(std::string &rData){
     std::string result = "";
-    char dataArray[rData.length()];
-    strcpy(dataArray, rData.c_str());
-    for(char i : dataArray)
-	result += i - 1;
+    char data_array[rData.length()];
+    strcpy(data_array, rData.c_str());
+    
+    char key_array[rData.length()];
+    strcpy(key_array, rData.c_str());
+
+    for(char i : data_array){
+    
+    }
+
     rData = result;
 }
--- crypto.h
+++ crypto.h
@@ -2,23 +2,28 @@
 #define CRYPT_H_WAPUMCKS
 
 #include <string>
+#include <vector>
 
 class Crypto{
 
 public:
-    Crypto(std::string key);
+    Crypto(std::string key = "", size_t keyLength = 4096);
     
-    void setKey(std::string key)    {mKey = key;}
+    void setKey(std::string key)	    {mKey = key;}
     
-    std::string getKey()	    {return mKey;}
+    std::string getKey()		    {return mKey;}
     
     void generateKey();
-
+    
+    std::vector<bool> toByte(char character);
+    char toChar(std::vector<bool> byte);
+    
     void encrypt(std::string &rData);
     void decrypt(std::string &rData);
 
 private:
     std::string mKey;
+    size_t	mKeyLength;
 };
 
 #endif /* end of include guard: CRYPT_H_WAPUMCKS */
