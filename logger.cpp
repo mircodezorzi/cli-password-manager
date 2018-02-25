4ed28aca30d9e1d2d4853848c0215035ac6a16f7
Mirco De Zorzi <mircodezorzi@gmail.com>
2018-02-25 20:56:21 +0100

    Added inline specifier to some functions. Almost completly finished the class

--- logger.cpp
+++ logger.cpp
@@ -5,31 +5,12 @@
 #include <string>
 #include <ctime>
 
-Logger::Logger(std::string logPath, size_t thresholdLevel, std::string messageFormat){ 
-    mPath   = logPath;
-    mLevel  = thresholdLevel;
-    mFormat = messageFormat;
+Logger::Logger(std::string filepath, size_t level, std::string format){ 
+    mPath   = filepath;
+    mLevel  = level;
+    mFormat = format;
 
     logStream.open(mPath, std::ios_base::app | std::ios_base::out);
-}
-
-void Logger::replace(std::string &str, std::string substr, std::string replacestr){
-    if(str.find(substr) != -1)
-	str.replace(str.find(substr), substr.length(), replacestr);
-}
-
-std::string Logger::getDate(std::string format){ 
-    time_t t = time(0);
-    struct tm *pCurrentDate = localtime(&t);    
-    
-    replace(format, "DD",   std::to_string(pCurrentDate->tm_mday));
-    replace(format, "MM",   std::to_string(pCurrentDate->tm_mon + 1));
-    replace(format, "YYYY", std::to_string(pCurrentDate->tm_year + 1900));
-    replace(format, "hh",   std::to_string(pCurrentDate->tm_hour));
-    replace(format, "mm",   std::to_string(pCurrentDate->tm_min));
-    replace(format, "ss",   std::to_string(pCurrentDate->tm_sec));
-
-    return format;
 }
 
 std::string Logger::formatMessage(std::string message){
@@ -41,26 +22,46 @@ std::string Logger::formatMessage(std::string message){
     return formattedMessage + "\n";
 }
 
-void Logger::log(size_t level, std::string message){
+std::string Logger::getDate(std::string format){ 
+    time_t t = time(0); 
+    struct tm *pCurrent = localtime(&t);    
+    
+    replace(format, "DD",   std::to_string(pCurrent->tm_mday));
+    replace(format, "MM",   std::to_string(pCurrent->tm_mon + 1));
+    replace(format, "YYYY", std::to_string(pCurrent->tm_year + 1900));
+    replace(format, "hh",   std::to_string(pCurrent->tm_hour));
+    replace(format, "mm",   std::to_string(pCurrent->tm_min));
+    replace(format, "ss",   std::to_string(pCurrent->tm_sec));
+
+    return format;
+}
+
+inline void Logger::replace(std::string& s, std::string ss, std::string rs){
+    if(s.find(ss) != -1)
+	s.replace(s.find(ss), ss.length(), rs);
+}
+
+// Different logging functions
+inline void Logger::log(size_t level, std::string message){
     if(level >= mLevel) logStream << formatMessage(message);
 }
 
-void Logger::debug(std::string message){
+inline void Logger::debug(std::string message){
     if(debugLevel >= mLevel) logStream << formatMessage(message);
 }
 
-void Logger::info(std::string message){
+inline void Logger::info(std::string message){
     if(infoLevel >= mLevel) logStream << formatMessage(message);
 }
 
-void Logger::warning(std::string message){
+inline void Logger::warning(std::string message){
     if(warningLevel >= mLevel) logStream << formatMessage(message);
 }
 
-void Logger::error(std::string message){
+inline void Logger::error(std::string message){
     if(errorLevel >= mLevel) logStream << formatMessage(message);
 }
 
-void Logger::critical(std::string message){
+inline void Logger::critical(std::string message){
     if(criticalLevel >= mLevel) logStream << formatMessage(message);
 }
--- logger.h
+++ logger.h
@@ -3,51 +3,52 @@
 
 #include <fstream>
 
-#define debugLevel 1
-#define infoLevel 2
-#define warningLevel 3
-#define errorLevel 4
+#define debugLevel    1
+#define infoLevel     2
+#define warningLevel  3
+#define errorLevel    4
 #define criticalLevel 5
 
 class Logger{
 
 public:
-    Logger(std::string logPath, size_t thresholdLevel = 0, \
-	   std::string messageFormat = "%(date) - %(time) - %(level) - %(msg)");
+    Logger(std::string filepath, size_t level = 0, \
+	   std::string format = "%(date) - %(time) | level: %(level) | %(msg)");
     
-    void setLevel(size_t thresholdLevel)      {mLevel  = thresholdLevel;}
-    void setFormat(std::string messageFormat) {mFormat = messageFormat;}
-    void setLogFilepath(std::string logPath)  {mPath   = logPath;}
+    void setLevel(size_t l)		{mLevel  = l;}
+    void setFormat(std::string f)	{mFormat = f;}
+    void setPath(std::string p)		{mPath   = p;}
 	
-    size_t	getLevel()		      {return mLevel;}
-    std::string getFormat()		      {return mFormat;}
-    std::string getLogPath()		      {return mPath;}
+    size_t	getLevel() const	{return mLevel;}
+    std::string getFormat() const       {return mFormat;}
+    std::string getPath() const		{return mPath;}
 
     // General logging function
-    void log(size_t level, std::string message);
+    inline void log(size_t level, std::string message);
   
     // Specific logging function with different priority levels
-    void debug(std::string message);
-    void info(std::string message);
-    void warning(std::string message);
-    void error(std::string message);
-    void critical(std::string message);
+    inline void debug(std::string message);
+    inline void info(std::string message);
+    inline void warning(std::string message);
+    inline void error(std::string message);
+    inline void critical(std::string message);
 
-private:
-    std::ofstream logStream;
-    
+private: 
     // Threshold for logger
     size_t mLevel;
     
     std::string mFormat;  
     std::string mPath;
 
-protected:
+protected: 
+    std::ofstream logStream;
+
     // Used to format message passed in log functions
     std::string formatMessage(std::string message);
-    std::string getDate(std::string format = "DD-MM-YYYY hh:mm:ss");
+    std::string getDate(std::string format = "DD/MM/YYYY hh:mm:ss");
     
-    void replace(std::string &str, std::string substr, std::string replacestr);
+    // s - > string | ss -> substring | rs -> replacement string 
+    inline void replace(std::string &s, std::string ss, std::string rs);
 
 };
 
