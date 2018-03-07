c480ab3ad0e23589640a1c41b5766aa4319c4824
Mirco De Zorzi <mircodezorzi@gmail.com>
2018-03-07 17:41:59 +0100

    Logger class finished. Ready to be used

--- logger.cpp
+++ logger.cpp
@@ -1,5 +1,4 @@
 #include "logger.h"
-#include "utils.cpp"
 
 #include <fstream>
 #include <ios>
@@ -25,37 +24,39 @@ std::string Logger::formatMessage(std::string message){
 
 std::string Logger::getDate(std::string format){ 
     time_t t = time(0); 
-    struct tm *pCurrent = localtime(&t);    
-    
+    struct tm *pCurrent = localtime(&t);        
     replace(format, "DD",   std::to_string(pCurrent->tm_mday));
     replace(format, "MM",   std::to_string(pCurrent->tm_mon + 1));
     replace(format, "YYYY", std::to_string(pCurrent->tm_year + 1900));
     replace(format, "hh",   std::to_string(pCurrent->tm_hour));
     replace(format, "mm",   std::to_string(pCurrent->tm_min));
     replace(format, "ss",   std::to_string(pCurrent->tm_sec));
-
     return format;
 }
 
-// Different logging functions
+inline void Logger::replace(std::string& str, std::string substr, std::string replacement){
+    if(str.find(substr) != -1)
+	str.replace(str.find(substr), substr.length(), replacement);
+}
+
 inline void Logger::log(size_t level, std::string message){
-    if(level >= mLevel) logStream << formatMessage(message);
+    if(level >= mLevel)		logStream << formatMessage(message);
 }
 
 inline void Logger::debug(std::string message){
-    if(debugLevel >= mLevel) logStream << formatMessage(message);
+    if(debugLevel >= mLevel)	logStream << formatMessage(message);
 }
 
 inline void Logger::info(std::string message){
-    if(infoLevel >= mLevel) logStream << formatMessage(message);
+    if(infoLevel >= mLevel)	logStream << formatMessage(message);
 }
 
 inline void Logger::warning(std::string message){
-    if(warningLevel >= mLevel) logStream << formatMessage(message);
+    if(warningLevel >= mLevel)	logStream << formatMessage(message);
 }
 
 inline void Logger::error(std::string message){
-    if(errorLevel >= mLevel) logStream << formatMessage(message);
+    if(errorLevel >= mLevel)	logStream << formatMessage(message);
 }
 
 inline void Logger::critical(std::string message){
--- logger.h
+++ logger.h
@@ -15,7 +15,7 @@ public:
     Logger(std::string filepath, size_t level = 0, \
 	   std::string format = "%(date) - %(time) | level: %(level) | %(msg)");
     
-    void setLevel(size_t l)		{mLevel  = l;}
+    void setLevel (size_t l)		{mLevel  = l;}
     void setFormat(std::string f)	{mFormat = f;}
     void setPath  (std::string p)	{mPath   = p;}
 	
@@ -27,16 +27,16 @@ public:
     inline void log(size_t level, std::string message);
   
     // Specific logging function with different priority levels
-    inline void debug(std::string message);
-    inline void info(std::string message);
-    inline void warning(std::string message);
-    inline void error(std::string message);
+    inline void debug	(std::string message);
+    inline void info	(std::string message);
+    inline void warning (std::string message);
+    inline void error	(std::string message);
     inline void critical(std::string message);
 
 private: 
     // Threshold for logger
-    size_t mLevel;
-    
+    size_t mLevel; 
+
     std::string mFormat;  
     std::string mPath;
 
@@ -45,7 +45,9 @@ protected:
 
     // Used to format message passed in log functions
     std::string formatMessage(std::string message);
-    std::string getDate(std::string format = "DD/MM/YYYY hh:mm:ss");
+    std::string getDate	     (std::string format = "DD/MM/YYYY - hh:mm:ss");
+    inline void replace	     (std::string &str, std::string substr,\
+			      std::string replacement);
 };
 
 #endif /* end of include guard: LOGGER_H_H3RB85EA */
