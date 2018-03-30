28df5602cd7475448bd4c5fffa114b4c1f578fd3
Mirco De Zorzi <mircodezorzi@gmail.com>
2018-03-30 22:46:46 +0200

    Removed junk code

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
-#include "databaseSystemManagment.h"
-#include "manager.h"
+#include "programManager.h"
 
 int main(){
-    //std::string filepath = "data.crypt";
-    //DatabaseSystemManagment dsm(filepath);
-    //dsm.import();
 
-    /*
-    Display display;
-    char input;
-    display.printFrame(0);
-    display.printBottomFrame();
-    display.printMiddle(0, "test");
-    display.setCursor(); 
-    do{
-	input = display.getInput();
-    }while(input != 'q');
-    */
-
+    ProgramManager pm("data.crypt");
+    pm.start();
+    
     /*
     std::string test = "1234";
     
@@ -31,12 +18,6 @@ int main(){
     std::cout << test << std::endl;
     */
 
-    Manager manager;
-    //manager.setFilepath("data.crypt");
-    //manager.readData();
-    //manager.writeData(); 
-    //std::cout << fileio::read("data");
-    manager.start();
     //Logger logger("out.log", 3);
     //logger.log(5, "this is just a test of level 5");
     //logger.debug("this is just some message");
