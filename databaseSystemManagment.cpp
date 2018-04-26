9dbdb5019e3a6874da87c32d707beb55486bfd3b
Mirco De Zorzi <mircodezorzi@gmail.com>
2018-04-26 20:38:42 +0200

    Added try-catch and logging functions in case of error

--- databaseSystemManagment.cpp
+++ databaseSystemManagment.cpp
@@ -1,14 +1,17 @@
 #include "databaseSystemManagment.h"
+
 #include "boost/variant.hpp"
 
 #include<vector>
 #include<string>
 #include<map>
-#include<fstream>
-#include<math.h>
-#include<algorithm>
 
-DatabaseSystemManagment::DatabaseSystemManagment(std::string filepath){
+#include<fstream>
+#include<algorithm>
+#include<math.h>
+
+DatabaseSystemManagment::DatabaseSystemManagment(std::string filepath)\
+    : Logger("log", 4){    
     mPath = filepath;
 }
 
@@ -20,7 +23,7 @@ std::string DatabaseSystemManagment::substring(std::string str, const char *sepa
 
 size_t DatabaseSystemManagment::find(std::string str, const char *c, size_t ordinal){
     size_t pos = -1;
-    for(int i = 0; i < ordinal; i++)
+    for(size_t i = 0; i < ordinal; i++)
 	pos = str.find(c, pos + 1);
     return pos;
 }
@@ -30,29 +33,20 @@ void DatabaseSystemManagment::removeSpaces(std::string &str){
 	str.erase(0, 1);
 }
 
+std::string DatabaseSystemManagment::multiply(char c, size_t n){
+    std::string result = "";
+    for(size_t i = 0; i < n; i++)
+	result += c;
+    return result;
+}
+
 void DatabaseSystemManagment::import(){ 
-    // This is probably the most contorted I've written in this project.
-    // The gist of it is that it checks line by line the specified file:
-    // - if the line starts with :: then that line will contain the table name
-    // - if the line starts with ;; then that line will contain the the fields
-    // - if the line starts with -- then then the table will be saved and the
-    //   program wil continue to read untill the end of the file, the field with 
-    //   a "!" before it will be the primary key. The key will also be stored in
-    //   mPrimaryKeys alongsite the name of the table where it is used, this will
-    //   be useful when sorting or querying the database
-    // - if the line doesn't start with the symbols previously listed, the program 
-    //   will start to read the data
-    //
-    //	    see example.db
-    //	    
     // TODO:
     //	    - add automatic counter if not present in file
-    //
     // BUGS:  
     //	    - multiple keys are not supported
     //	    - attributes cannot contain double quotes
     //	    - table name cannot be "data"
-
 
 			  std::map<std::string,	boost::variant<std::string, size_t, double, bool>> record;
     std::map<std::string, std::map<std::string, boost::variant<std::string, size_t, double, bool>>> table;
@@ -61,44 +55,46 @@ void DatabaseSystemManagment::import(){
     std::vector<std::string> fields;
     std::string tableName, primaryKey, id;
     
-    for(std::string row; std::getline(inputFile, row);){
-	if(row.substr(0, 2) == "::")		 	// table name
-	    tableName = substring(row, "\"");
-	else if(row.substr(0, 2) == ";;"){	        // fields
-	    primaryKey = "";
-	    fields.clear();
-	    while(!row.empty()){
-		removeSpaces(row);
-		if(row.substr(0, 1) == "!"){		// primary key
-		    primaryKey = substring(row, "\"");
-		    mPrimaryKeys[tableName] = primaryKey; 
+    try{
+	for(std::string row; std::getline(inputFile, row);){
+	    if(row.substr(0, 2) == "::")     	 // table name
+		tableName = substring(row, "\"");
+	    else if(row.substr(0, 2) == ";;"){	 // fields
+		primaryKey = "";
+		fields.clear();
+		while(!row.empty()){
+		    removeSpaces(row);
+		    if(row.substr(0, 1) == "!"){ // primary key
+			primaryKey = substring(row, "\"");
+			mPrimaryKeys[tableName] = primaryKey; 
+		    }
+		    fields.push_back(substring(row, "\""));	
+		    row.erase(0, find(row, "\"", 2) + 1);
 		}
-		fields.push_back(substring(row, "\""));	
-		row.erase(0, find(row, "\"", 2) + 1);
+	    }else if(row.substr(0, 2) == "--"){	// table end	
+		mTables[tableName] = table;
+		table.clear();
+	    }else{		    		// data
+		record.clear();
+		for(int i = 0; i < fields.size(); i++){
+		    if(fields[i] == primaryKey)
+			id =  substring(row, "\"");
+		    else
+			record[fields[i]] = substring(row, "\"");
+		    row.erase(0, find(row, "\"", 2) + 1);	
+		}
+		table[id] = record;
 	    }
-	}else if(row.substr(0, 2) == "--"){		// table end	
-	    mTables[tableName] = table;
-	    table.clear();
-	}else{		    				// data
-	    record.clear();
-	    for(int i = 0; i < fields.size(); i++){
-		if(fields[i] == primaryKey)
-		    id =  substring(row, "\"");
-		else
-		    record[fields[i]] = substring(row, "\"");
-		row.erase(0, find(row, "\"", 2) + 1);	
-	    }
-	    table[id] = record;
 	}
-    } 
+    }catch(...){
+	critical("Error importing database");
+	throw std::runtime_error("Error importing database");
+    }
 }
 
-auto DatabaseSystemManagment::query(std::string tableName, std::string recordKey){
+std::map<std::string, boost::variant<std::string, size_t, double, bool>> DatabaseSystemManagment::query(std::string tableName, std::string recordKey){
     for(auto table: mTables)
 	for(auto record: table.second)
 	    if(record.first == recordKey)
 		return record.second;
-}
-
-auto DatabaseSystemManagment::query(std::string tableName, std::vector<std::string> fields){
 }
--- databaseSystemManagment.h
+++ databaseSystemManagment.h
@@ -1,13 +1,14 @@
 #ifndef DATABASESYSTEMMANAGMENT_H_4WMQHENX
 #define DATABASESYSTEMMANAGMENT_H_4WMQHENX
 
+#include "logger.h"
 #include "boost/variant.hpp"
 
 #include<vector>
 #include<string>
 #include<map>
 
-class DatabaseSystemManagment{
+class DatabaseSystemManagment : protected Logger{
 
 public:
     DatabaseSystemManagment(std::string filepath);
@@ -18,11 +19,10 @@ public:
      
     void import();
     
-    auto query()		    {return mTables;}
-    auto query(std::string tableName, std::string recordKey);
-    auto query(std::string tableName, std::vector<std::string> fields);
-    
-    auto sort(std::string tableName, std::string fields);
+    std::map<std::string, boost::variant<std::string, size_t, double, bool>>\
+	 query(std::string tableName, std::string recordKey);
+    auto query(std::string tableName, std::vector<std::string> fields); 
+    auto query() {return mTables;}
 
 private:
     std::map<std::string, std::map<std::string, std::map<std::string,
@@ -30,8 +30,9 @@ private:
     std::map<std::string, std::string> mPrimaryKeys;
 
 protected:
+    std::string multiply(char c, size_t n);
     std::string substring(std::string str, const char *separator);
-    size_t	find(std::string str, const char *c, size_t ordinal);	
+    size_t	find(std::string str, const char *c, size_t ordinal);
     void	removeSpaces(std::string &str);
 
     std::string mPath;
