8d68c60415b98bd6e92430dc1ff3d99843cd22af
Mirco De Zorzi <mircodezorzi@gmail.com>
2018-05-06 20:23:50 +0200

    Added missing double quotes exception

--- exceptions.h
+++ exceptions.h
@@ -1,4 +1,4 @@
 #define tableNameNotSet	   "Expected table name at line %(line); please use \"::\" at start of row to set table name."
 #define tableFieldsNotSet  "Expected fields at line %(line); please use \";;\" at start of row to set table fileds."
 #define primaryKeyOverload "At line %(line): database system manager only supports one primary key. Setting first flag as primary key."
-
+#define missingDoubleQuotes "Missing double quotes at line %(line). Fixing problem automatically."
